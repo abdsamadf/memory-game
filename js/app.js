7242e82bc0a9e5312efcbd045dddb8783aeb9fb7
abdsamadf <abdulsamadf97@gmail.com>
2019-07-19 12:27:14 +0500

    Feat: Implement the matching logic
    Matching logic includes player guesses correctly or incorrectly. If two cards match, they stay turned over. If two cards do not match, they turn back.

--- js/app.js
+++ js/app.js
@@ -2,14 +2,11 @@
  * Create a list that holds all of your cards
  */
 let cardsList = document.querySelectorAll(".card");
-console.log(cardsList);
 
 let numbers = [];
 for (let i = 0; i < cardsList.length; i++) {
     numbers.push(i);
 }
-console.log(numbers);
-
 
 /*
  * Display the cards on the page
@@ -35,16 +32,13 @@ function shuffle(array) {
 
 // shuffle the list of cards
 let randomNumbers = shuffle(numbers);
-console.log(randomNumbers);
 let randomCards = [];
 for (let i = 0; i < cardsList.length; i++) {
     randomCards.push(cardsList[randomNumbers[i]]);
 }
-console.log(randomCards);
 
 // remove cards
 let deck = document.querySelector('.deck');
-console.log(deck);
 while (deck.firstChild) {
     deck.removeChild(deck.firstChild);
 }
@@ -69,15 +63,70 @@ randomCards.forEach(element => {
 
 // functionality to handle clicks on cards
 deck.addEventListener('click', (evt) => {
-    let targetElement = evt.target;
-    if (targetElement.nodeName.toLowerCase() === 'li') {
-        console.log(targetElement.classList);
-        superToggle(targetElement, "open", "show");
-    }
+    cardMatchingLogic(evt);
 })
 
+// list of open cards
+let openCards = [];
+// classes for card turn it over
+const cls = ["open", "show"];
+
+/**
+ * Show the card
+ * @param evt
+ */
+function cardMatchingLogic(evt) {
+    let targetElement = evt.target;
+
+    cardFlip(targetElement);
+
+    // list already has another card, check to see if the two cards match or not
+    if (openCards.length > 1) {
+        // cards do match, lock the cards in the open position
+        if (openCards[0].firstElementChild.classList[1] === openCards[1].firstElementChild.classList[1]) {
+            cardsMatch();
+        }
+        // cards do not match, remove the cards from the list and hide the card's symbol
+        if (openCards[0].firstElementChild.classList[1] !== openCards[1].firstElementChild.classList[1]) {
+            cardsMismatch();
+        }
+    }
+}
 
 let superToggle = (element, class0, class1) => {
     element.classList.toggle(class0);
     element.classList.toggle(class1);
+}
+/**
+ * Flip the cards and store the turned-over cards in openCards list
+ * @param targetElement
+ */
+function cardFlip(targetElement) {
+    if (targetElement.nodeName.toLowerCase() === 'li') {
+        if (!targetElement.classList.contains(...cls)) {
+            superToggle(targetElement, ...cls);
+            openCards.push(targetElement);
+        }
+    }
+}
+/**
+ * cards do not match, remove the cards from the list and hide the card's symbol
+ */
+function cardsMismatch() {
+    setTimeout(() => {
+        superToggle(openCards[0], "open", "show");
+        superToggle(openCards[1], "open", "show");
+        openCards.splice(0, 2);
+    }, 1000);
+}
+
+/**
+ * cards do match, lock the cards in the open position
+ */
+function cardsMatch() {
+    openCards[0].classList.remove(...cls);
+    openCards[1].classList.remove(...cls);
+    openCards[0].classList.add("match");
+    openCards[1].classList.add("match");
+    openCards.splice(0, 2);
 }
