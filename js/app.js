60875694cd486949ebd4af4625954ebb63a9edfd
abdsamadf <abdulsamadf97@gmail.com>
2019-07-24 23:39:25 +0500

    update app.js
    modify commenting

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-/*
- * Create a list that holds all of your cards
+/**
+ * list cards
  */
 let cardsList = ["fa-anchor", "fa-anchor",
                 "fa-bolt", "fa-bolt",
@@ -17,13 +17,6 @@ let cardsList = ["fa-anchor", "fa-anchor",
 function buildCard(i) {
     return `<li class="card"><i class="fa ${i}"></i></li>`;
 }
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -70,16 +63,6 @@ const movesFactor = 9;
 const cls = ["open", "show"];
 
 initGame();
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
 
 // functionality to handle clicks on cards
 deck.addEventListener('click', (evt) => {
